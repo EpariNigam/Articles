c35554c2af94b3252c414867151c8cb98e7374cb
Nigam Patro <nigam.patro2010@gmail.com>
2020-10-06 22:06:35 +0530

    - Added dependencies  - Designed layout.

--- app/src/main/java/com/nigam/articles/MainActivity.kt
+++ app/src/main/java/com/nigam/articles/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.nigam.articles
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.nigam.articles.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setSupportActionBar(binding.toolbar)
     }
 }
